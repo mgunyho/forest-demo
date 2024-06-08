7fffaf8da04a9685e43e5280b999fbbb3467ff21
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2024-06-08 20:50:19 +0300

    Add final target

--- 3DtreeDemo.js
+++ 3DtreeDemo.js
@@ -7,12 +7,14 @@ let cam
 let targetZ = 0
 let timeToNextTarget = 4
 
+let endTarget
+
 let sinTime = 0
 let deltaTime = 0
 
 let introFont
 
-const treeSpawnAmount = 30
+const treeSpawnAmount = 80
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -46,15 +48,18 @@ function draw() {
    cam.linearAdvance(demoTime, yOffset)
 
   //Every time the camera meets target, move target and spawn trees
-  if ( cam.atTarget ) {
+  if ( cam.atTarget && !endTarget ) {
     if (  demoTime > 16 ) {
       timeToNextTarget = 2
+    }
+    if ( demoTime > 52 ) {
+      timeToNextTarget = 32
+      endTarget = createVector( 0, 0, cam.target.z - 6400 )
     }
 
     moveCameraTarget()
     objectAdderDeleter()
   }
-
   
   //Add lead dissort after 16 beats
   let dissort = 0
@@ -66,7 +71,7 @@ function draw() {
   const defFov = 2 * Math.atan(height / 2 / 800)
   perspective(1, width/height, 10)
   //wonky perspective effect at 32 beats
-  if (demoTime > 32) {
+  if (demoTime > 32 && demoTime < 52 ) {
     sinTime = demoTime - 32
     perspective(3*(Math.sin(sinTime)/10) + 2, width/height, 10)
   }
@@ -74,7 +79,13 @@ function draw() {
   //Draw stuff
   background(20, 20, 40); // Sky blue
 
-  drawIntroText()
+  if (demoTime < 8 ) {
+    drawIntroText()
+  }
+
+  if (endTarget) {
+    drawEndTarget()
+  }
 
   drawGround()
 
@@ -84,6 +95,7 @@ function draw() {
   for (const blob of blobs) {
     blob.draw()
   }
+  
 }
 
 function moveCameraTarget() {
@@ -93,7 +105,11 @@ function moveCameraTarget() {
   targetZ = targetZ - 800
   const newTarget = createVector( x, 0, cam.target.z - 800 )
 
-  cam.setTarget(newTarget, lastTargetTime + timeToNextTarget, lastTargetTime)
+  if ( !endTarget ) {
+    cam.setTarget(newTarget, lastTargetTime + timeToNextTarget, lastTargetTime)
+  } else {
+    cam.setTarget(endTarget, lastTargetTime + timeToNextTarget, lastTargetTime)
+  }
 }
 
 function objectAdderDeleter() {
@@ -104,6 +120,16 @@ function objectAdderDeleter() {
     //Prune trees that are behind camera Z
     trees = trees.filter( tree => tree.z - 50 < cam.location.z )
     blobs = blobs.filter( blobs => blobs.z - 50 < cam.location.z )
+}
+
+function drawEndTarget() {
+  push();
+  colorMode(HSL)
+  fill(0, 0, 100);
+  translate(endTarget);
+  translate(0, 0, -200);
+  cylinder(400, 400)
+  pop();
 }
 
 function drawIntroText() {
